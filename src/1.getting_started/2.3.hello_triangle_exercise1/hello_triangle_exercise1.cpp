d0dec61c804aae12c66f5e4e47ab4da409429b79
timtingwei <timtingwei@hotmail.com>
2017-12-25 13:23:56 +0800

    add traiangle first exercise

---
+++ src/1.getting_started/2.3.hello_triangle_exercise1/hello_triangle_exercise1.cpp
@@ -0,0 +1,203 @@
+// Copyright <2017>[Tim Hu]
+// 添加更多顶点到数据中，使用glDrawArrays，尝试绘制两个彼此相连的三角形
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+// #include <GL/glut.h>
+
+#include <iostream>
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key,
+                  int scancode, int action, int mode);
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// 顶点着色器
+const GLchar* vertexShaderSource = "#version 330 core\n"
+  "layout (location = 0) in vec3 position;\n"
+  "void main() {\n"
+  "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+  "}\n\0";
+
+// 片段着色器
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main() {\n"
+    "color = vec4(1.0f, 0.5, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+// 实例化GLFW窗口
+int main() {
+  std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+  // Init GLFW
+  glfwInit();
+  // Sel all the required options for GLFW
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL",
+                                        nullptr, nullptr);
+  if (window == nullptr) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  // 设置当前的上下文
+  glfwMakeContextCurrent(window);
+  // 注册函数至适合的回调
+  glfwSetKeyCallback(window, key_callback);
+
+
+  // 初始化GLEW
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    std::cout << "Failed to initialize GLEW" << std::endl;
+    return -1;
+  }
+  // 设置窗口维度(Dimension)
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
+
+  // 建立和编译着色器
+  // 创建顶点着色器
+  GLuint vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  // 源码附加到着色器对象上
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+  // 检测glComplieShader是否编译成功
+  GLint success;
+  GLchar infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // 创建片段着色器
+  GLuint fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  // 源码附加到着色器对象上
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+  // 检测glComplieShader是否编译成功
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // 把两个着色器对象链接到用来渲染的着色程序中
+  GLuint shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infoLog << std::endl;
+  }
+  // 删除着色器
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // 设置顶点数据和属性指针
+  // 顶点输入, 绘制两个彼此相连的三角形
+  GLfloat vertices[] = {
+    // 第一个triangle
+    0.5f,   0.5f, 0.0f,    // 右上角
+    0.5f,  -0.5f, 0.0f,    // 右下角
+    0.0f,   0.0f, 0.0f,    // 连接处
+    // 第二个triangle
+    -0.5f, -0.5f, 0.0f,    // 左下角
+    -0.5f,  0.5f, 0.0f,    // 左上角
+    0.0f,   0.0f, 0.0f     // 连接处
+  };
+
+  GLuint indices[] = {     // 索引从0开始
+    0, 1, 2,               // 第一个三角形
+    3, 4, 2                // 第二个三角形
+  };
+
+  // 生成一个VBO顶点对象，VBO顶点数组对象,EBO索引缓冲对象
+  GLuint VBO, VAO, EBO;
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
+  // 先绑定顶点数组对象，再绑定和设置顶点缓存和属性指针
+  glBindVertexArray(VAO);
+
+  // 把顶点数组复制到缓存中供OpenGL使用
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),
+               vertices, GL_STATIC_DRAW);
+
+  // Index Buffer Object绑定和复制
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),
+               indices, GL_STATIC_DRAW);
+
+  // 设置顶点属性指针
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+                        3 * sizeof(GLfloat), (GLvoid*)0);
+  glEnableVertexAttribArray(0);
+
+  // 绑定到GL_ARRAY_BUFFER目标上
+  /*Note that this is allowed, the call to glVertexAttribPointer registered VBO
+    as the currently bound vertex buffer object, so afterwards we can safely unbind*/
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);  // Unbind VAO
+
+  // 解绑定EBO
+  // 不注释这行代码会以线框模式绘制
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // 实现简单的游戏循环
+  while (!glfwWindowShouldClose(window)) {   // 要求被退出时函数返回True
+    // 是否触发事件
+    glfwPollEvents();
+
+    // Render
+    // 清空颜色缓冲
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw first triangles
+    // 激活程序对象
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);  // 从索引0开始依次绘制6个顶点
+    // 从索引缓冲渲染
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // 交换screen buffer
+    glfwSwapBuffers(window);
+  }
+  // Properly de-allocate all resources once they've outlived their purpose
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+
+  // 释放GLFW分配的内存
+  glfwTerminate();
+  return 0;
+}
+
+void key_callback(GLFWwindow* window, int key,
+                  int scancode, int action, int mode) {
+  // 当用户按下esc键, 我们设置window窗口的windowShouldCloseni属性为True
+  // 关闭应用程序
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+// ..
